d4654672c04d0c8756f1796d53d5839e78113671
Fabian Vergara <vergarf@gmail.com>
2016-05-12 20:57:04 -0400

    Fixed keyboard bug

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -8,17 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet var labelQuestion: UILabel!
+    @IBOutlet var textField: UITextField!
+    
+    @IBAction func submitBtn(sender: AnyObject) {
+        
+        labelQuestion.text = textField.text
+        
+        print("Tap of press me/ submit button")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print("Hello Fabian") //Print to console
+        
+        self.textField.delegate = self
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField!) -> Bool{
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    
 
 
 }
